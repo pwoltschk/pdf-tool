66b8f6dd2b40364595ab1377684a24b5a9759775
wopa <p.woltschkow@gmail.com>
2021-09-21 16:38:07 +0200

    Projektdateien hinzufügen.

---
+++ PdfTool/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace PdfTool
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var processor = new SplitProcessor().SplitAsync("C:\\Users\\wopa\\Documents\\starvpn.pdf");
+
+
+
+        }
+    }
+}
---
+++ PdfTool/SplitProcessor.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using iText.Kernel.Pdf;
+using Path = System.IO.Path;
+
+namespace PdfTool
+{
+    internal class SplitProcessor
+    {
+        public async Task SplitAsync(string fullPath)
+        {
+            var directory = Path.GetDirectoryName(fullPath);
+
+            var outputDirectory  = Path.Combine(directory, Path.GetFileNameWithoutExtension(fullPath));
+
+            Directory.CreateDirectory(outputDirectory);
+
+            using (PdfDocument pdfDocument = new PdfDocument(new PdfReader(fullPath)))
+            {
+                for (int page = 1; page <= pdfDocument.GetNumberOfPages(); page++)
+                {
+                    string outputPdfPath = Path.Combine(outputDirectory, $"page_{page}.pdf");
+
+                    using (PdfDocument outputDocument = new PdfDocument(new PdfWriter(outputPdfPath)))
+                    {
+                        pdfDocument.CopyPagesTo(page, page, outputDocument);
+                    }
+                }
+            }
+        }
+    }
+}
