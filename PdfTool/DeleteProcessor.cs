e4d97bca87447c60bbceef7f341c7d5f12dd78a0
wopa <p.woltschkow@gmail.com>
2021-11-26 17:45:01 +0200

    init delete processor

---
+++ PdfTool/DeleteProcessor.cs
@@ -0,0 +1,21 @@
+﻿using iText.Kernel.Pdf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PdfTool
+{
+    internal class DeleteProcessor
+    {
+        public void Delete(string inputPdfPath, string outputPdfPath, int pageToDelete)
+        {
+            using (PdfDocument pdfDocument = new PdfDocument(new PdfReader(inputPdfPath), new PdfWriter(outputPdfPath)))
+            {
+                pdfDocument.GetPage(pageToDelete).GetPdfObject().Remove();
+            }
+        }
+    }
+}
+
