6dba1f4fa365e106343ce38f7966304dcd377e33
wopa <p.woltschkow@gmail.com>
2021-12-10 18:34:56 +0200

    implemented range deletion

--- PdfTool/DeleteProcessor.cs
+++ PdfTool/DeleteProcessor.cs
@@ -2,12 +2,7 @@
 using iText.Kernel.Pdf;
 using iText.Kernel.Pdf.Canvas;
 using iText.Kernel.Pdf.Xobject;
-using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Path = System.IO.Path;
 
 namespace PdfTool
@@ -18,6 +13,19 @@ namespace PdfTool
         {
             var outputPdfPath = $"{Path.GetDirectoryName(inputPdfPath)}/{Path.GetFileNameWithoutExtension(inputPdfPath)}_removedPage{pageToDelete}.pdf";
 
+            DeletePages(inputPdfPath, outputPdfPath, pageToDelete);
+        }
+
+        public void Delete(string inputPdfPath, int fromPage, int toPage)
+        {
+            var pagesToDelete = Enumerable.Range(fromPage, toPage - fromPage + 1);
+            var outputPdfPath = $"{Path.GetDirectoryName(inputPdfPath)}/{Path.GetFileNameWithoutExtension(inputPdfPath)}_removedPage{fromPage}-{toPage}.pdf";
+
+            DeletePages(inputPdfPath, outputPdfPath, pagesToDelete.ToArray());
+        }
+
+        private static void DeletePages(string inputPdfPath, string outputPdfPath, params int[] pages)
+        {
             using (PdfDocument inputPdfDocument = new PdfDocument(new PdfReader(inputPdfPath)))
             using (PdfDocument outputPdfDocument = new PdfDocument(new PdfWriter(outputPdfPath)))
             {
@@ -25,7 +33,7 @@ namespace PdfTool
 
                 for (int page = 1; page <= pageCount; page++)
                 {
-                    if (page != pageToDelete)
+                    if (!pages.Contains(page))
                     {
                         PdfPage pdfPage = inputPdfDocument.GetPage(page);
                         PdfFormXObject pageCopy = pdfPage.CopyAsFormXObject(outputPdfDocument);
