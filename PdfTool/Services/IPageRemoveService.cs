9d515af92bf7d639e6cfcdcd179655808bcdd165
wopa <p.woltschkow@gmail.com>
2021-12-14 19:45:01 +0200

    carveout page remove logic

---
+++ PdfTool/Services/IPageRemoveService.cs
@@ -0,0 +1,7 @@
+﻿namespace PdfTool.Services
+{
+    internal interface IPageRemoveService
+    {
+        void RemovePages(string inputPdfPath, string outputPdfPath, params int[] pages);
+    }
+}
---
+++ PdfTool/Services/PageRemoveService.cs
@@ -0,0 +1,34 @@
+﻿using iText.Kernel.Geom;
+using iText.Kernel.Pdf.Canvas;
+using iText.Kernel.Pdf.Xobject;
+using iText.Kernel.Pdf;
+using System;
+using System.Linq;
+
+namespace PdfTool.Services
+{
+    internal class PageRemoveService : IPageRemoveService
+    {
+        public void RemovePages(string inputPdfPath, string outputPdfPath, params int[] pages)
+        {
+            using (PdfDocument inputPdfDocument = new PdfDocument(new PdfReader(inputPdfPath)))
+            using (PdfDocument outputPdfDocument = new PdfDocument(new PdfWriter(outputPdfPath)))
+            {
+                int pageCount = inputPdfDocument.GetNumberOfPages();
+
+                for (int page = 1; page <= pageCount; page++)
+                {
+                    if (!pages.Contains(page))
+                    {
+                        PdfPage pdfPage = inputPdfDocument.GetPage(page);
+                        PdfFormXObject pageCopy = pdfPage.CopyAsFormXObject(outputPdfDocument);
+                        Rectangle rectangle = pdfPage.GetPageSize();
+                        PageSize pageSize = new PageSize(rectangle);
+                        outputPdfDocument.AddNewPage(pageSize);
+                        new PdfCanvas(outputPdfDocument.GetLastPage()).AddXObjectAt(pageCopy, 0, 0);
+                    }
+                }
+            }
+        }
+    }
+}
