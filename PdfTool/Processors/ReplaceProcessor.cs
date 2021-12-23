2a11dc1bc79ae5eef20bea1fedebcb74b7f3748f
wopa <p.woltschkow@gmail.com>
2021-12-23 18:12:34 +0200

    scaffold replace processor

---
+++ PdfTool/Processors/ReplaceProcessor.cs
@@ -0,0 +1,13 @@
+﻿
+using Path = System.IO.Path;
+
+namespace PdfTool.Processors
+{
+    internal class ReplaceProcessor
+    {
+        public void Replace(string inputPdfPath, int page1, int page2)
+        {
+            var outputPdfPath = $"{Path.GetDirectoryName(inputPdfPath)}/{Path.GetFileNameWithoutExtension(inputPdfPath)}_replacedPage{page1}with{page2}.pdf";
+        }
+    }
+}
