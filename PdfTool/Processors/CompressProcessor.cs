0a0e91fa08b1b2730ecdd259b3bd1026af060958
wopa <p.woltschkow@gmail.com>
2022-02-06 20:12:34 +0200

    create compression processor

---
+++ PdfTool/Processors/CompressProcessor.cs
@@ -0,0 +1,24 @@
+﻿using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+
+namespace PdfTool.Processors
+{
+    internal class CompressProcessor
+    {
+        public static void Compress(string inputPath, string outputPath)
+        {
+            using (PdfDocument pdfDocument = new PdfDocument(new PdfReader(inputPath), new PdfWriter(outputPath)))
+            {
+                pdfDocument.GetWriter().SetCompressionLevel(CompressionConstants.BEST_COMPRESSION);
+
+                pdfDocument.GetWriter().SetCloseStream(false);
+
+                Document doc = new Document(pdfDocument);
+                doc.Add(new Paragraph("Compressed with PDFTool Cli"));
+
+                doc.Close();
+            }
+        }
+    }
+}
