457379ad3b4f7aaa428beabc62c84b9d24f38628
wopa <p.woltschkow@gmail.com>
2022-02-13 17:23:45 +0200

    add outputpath to compressed processor

--- PdfTool/Processors/CompressProcessor.cs
+++ PdfTool/Processors/CompressProcessor.cs
@@ -1,24 +1,25 @@
 ﻿using iText.Kernel.Pdf;
 using iText.Layout;
 using iText.Layout.Element;
+using Path = System.IO.Path;
 
 namespace PdfTool.Processors
 {
     internal class CompressProcessor
     {
-        public static void Compress(string inputPath, string outputPath)
+        public static void Compress(string inputPdfPath)
         {
-            using (PdfDocument pdfDocument = new PdfDocument(new PdfReader(inputPath), new PdfWriter(outputPath)))
-            {
-                pdfDocument.GetWriter().SetCompressionLevel(CompressionConstants.BEST_COMPRESSION);
+            var outputPdfPath = $"{Path.GetDirectoryName(inputPdfPath)}/{Path.GetFileNameWithoutExtension(inputPdfPath)}_compressed.pdf";
 
-                pdfDocument.GetWriter().SetCloseStream(false);
+            using PdfDocument pdfDocument = new PdfDocument(new PdfReader(inputPdfPath), new PdfWriter(outputPdfPath));
+            pdfDocument.GetWriter().SetCompressionLevel(CompressionConstants.BEST_COMPRESSION);
 
-                Document doc = new Document(pdfDocument);
-                doc.Add(new Paragraph("Compressed with PDFTool Cli"));
+            pdfDocument.GetWriter().SetCloseStream(false);
 
-                doc.Close();
-            }
+            Document doc = new Document(pdfDocument);
+            doc.Add(new Paragraph("Compressed with PDFTool Cli"));
+
+            doc.Close();
         }
     }
 }
