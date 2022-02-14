0034c8d18e3126a784d28819d91c5efe00cea713
wopa <p.woltschkow@gmail.com>
2022-02-14 18:34:56 +0200

    added smart mode to compression

--- PdfTool/Processors/CompressProcessor.cs
+++ PdfTool/Processors/CompressProcessor.cs
@@ -1,6 +1,5 @@
 ﻿using iText.Kernel.Pdf;
-using iText.Layout;
-using iText.Layout.Element;
+using System.Collections.Generic;
 using Path = System.IO.Path;
 
 namespace PdfTool.Processors
@@ -9,17 +8,23 @@ namespace PdfTool.Processors
     {
         public static void Compress(string inputPdfPath)
         {
+
             var outputPdfPath = $"{Path.GetDirectoryName(inputPdfPath)}/{Path.GetFileNameWithoutExtension(inputPdfPath)}_compressed.pdf";
 
-            using PdfDocument pdfDocument = new PdfDocument(new PdfReader(inputPdfPath), new PdfWriter(outputPdfPath));
-            pdfDocument.GetWriter().SetCompressionLevel(CompressionConstants.BEST_COMPRESSION);
+            using PdfDocument pdfDocument = new PdfDocument(new PdfReader(inputPdfPath));
+            using PdfDocument pdfDocOptimized = new PdfDocument(new PdfWriter(outputPdfPath));
+            pdfDocOptimized.GetWriter().SetCompressionLevel(CompressionConstants.BEST_COMPRESSION);
+            pdfDocOptimized.GetWriter().SetSmartMode(true);
 
-            pdfDocument.GetWriter().SetCloseStream(false);
+            HashSet<PdfObject> usedObjects = new HashSet<PdfObject>();
 
-            Document doc = new Document(pdfDocument);
-            doc.Add(new Paragraph("Compressed with PDFTool Cli"));
+            for (int page = 1; page <= pdfDocument.GetNumberOfPages(); page++)
+            {
+                PdfPage pdfPage = pdfDocument.GetPage(page);
+                usedObjects.Add(pdfPage.GetPdfObject());
 
-            doc.Close();
+                pdfDocOptimized.AddPage(pdfDocument.GetPage(page).CopyTo(pdfDocOptimized));
+            }
         }
     }
 }
