c4a1c54ec272b27aac3c908ff95bc32478cb37b2
wopa <p.woltschkow@gmail.com>
2022-02-15 19:45:01 +0200

    added full compression mode on writer

--- PdfTool/Processors/CompressProcessor.cs
+++ PdfTool/Processors/CompressProcessor.cs
@@ -1,5 +1,4 @@
 ﻿using iText.Kernel.Pdf;
-using System.Collections.Generic;
 using Path = System.IO.Path;
 
 namespace PdfTool.Processors
@@ -8,20 +7,20 @@ namespace PdfTool.Processors
     {
         public static void Compress(string inputPdfPath)
         {
-
             var outputPdfPath = $"{Path.GetDirectoryName(inputPdfPath)}/{Path.GetFileNameWithoutExtension(inputPdfPath)}_compressed.pdf";
 
+            var writerProperties = new WriterProperties();
+            writerProperties.SetFullCompressionMode(true);
+            writerProperties.SetCompressionLevel(CompressionConstants.BEST_COMPRESSION);
+
             using PdfDocument pdfDocument = new PdfDocument(new PdfReader(inputPdfPath));
-            using PdfDocument pdfDocOptimized = new PdfDocument(new PdfWriter(outputPdfPath));
-            pdfDocOptimized.GetWriter().SetCompressionLevel(CompressionConstants.BEST_COMPRESSION);
+            using PdfDocument pdfDocOptimized = new PdfDocument(new PdfWriter(outputPdfPath, writerProperties));
             pdfDocOptimized.GetWriter().SetSmartMode(true);
 
-            HashSet<PdfObject> usedObjects = new HashSet<PdfObject>();
 
             for (int page = 1; page <= pdfDocument.GetNumberOfPages(); page++)
             {
                 PdfPage pdfPage = pdfDocument.GetPage(page);
-                usedObjects.Add(pdfPage.GetPdfObject());
 
                 pdfDocOptimized.AddPage(pdfDocument.GetPage(page).CopyTo(pdfDocOptimized));
             }
