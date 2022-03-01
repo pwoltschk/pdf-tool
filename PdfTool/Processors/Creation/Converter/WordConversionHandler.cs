3fb538d889ad3b2319edd8ca5b160a8585500bb8
wopa <p.woltschkow@gmail.com>
2022-03-01 19:23:45 +0200

    remove microsoft interop implementation

--- PdfTool/Processors/Creation/Converter/WordConversionHandler.cs
+++ PdfTool/Processors/Creation/Converter/WordConversionHandler.cs
@@ -1,33 +1,11 @@
-﻿using Microsoft.Office.Interop.Word;
-using System.IO;
-
-using Word = Microsoft.Office.Interop.Word;
-
-namespace PdfTool.Processors.Creation.Converter
+﻿namespace PdfTool.Processors.Creation.Converter
 {
     internal class WordConversionHandler : IConversionHandler
     {
         public void Convert(string inputString)
         {
-            string pdfPath = Path.ChangeExtension(inputString, ".pdf");
 
-            Word.Application wordApp = new Word.Application();
-            wordApp.Visible = false;
-            Document wordDoc = null;
 
-            try
-            {
-                wordDoc = wordApp.Documents.Open(pdfPath);
-                wordDoc.SaveAs2(pdfPath, WdSaveFormat.wdFormatPDF);
-            }
-            finally
-            {
-                if (wordApp != null)
-                {
-                    wordApp.Quit();
-                    System.Runtime.InteropServices.Marshal.ReleaseComObject(wordApp);
-                }
-            }
         }
     }
 }
