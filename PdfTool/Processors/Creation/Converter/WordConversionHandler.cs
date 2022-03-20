4ea0ea4601c02df5f97410318f4986f1e4b8facf
wopa <p.woltschkow@gmail.com>
2022-03-20 18:12:34 +0200

    remove libre handler

--- PdfTool/Processors/Creation/Converter/WordConversionHandler.cs
+++ PdfTool/Processors/Creation/Converter/WordConversionHandler.cs
@@ -1,4 +1,7 @@
-﻿using PdfTool.Processors.Creation.Converter.Libre;
+﻿using System.Diagnostics;
+using System.Reflection;
+using System;
+using System.IO;
 
 namespace PdfTool.Processors.Creation.Converter
 {
@@ -6,7 +9,42 @@ namespace PdfTool.Processors.Creation.Converter
     {
         public void Convert(string inputString)
         {
-            new LibreProcessExecutor().ConvertToPdf(inputString);
+            var libreOfficePath = GetApplicationPath();
+
+            var procStartInfo = new ProcessStartInfo(libreOfficePath, $"--convert-to pdf --nologo --headless --outdir {Path.GetDirectoryName(inputString)} {inputString}")
+            {
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                WorkingDirectory = Environment.CurrentDirectory
+            };
+
+            using var process = new Process { StartInfo = procStartInfo };
+            process.Start();
+            process.WaitForExit();
+
+            if (process.ExitCode != 0)
+            {
+                throw new Exception($"Application exited with Error Code: {process.ExitCode}");
+            }
+        }
+
+        private static string GetApplicationPath()
+        {
+            string path;
+            switch (Environment.OSVersion.Platform)
+            {
+                case PlatformID.Unix:
+                    path = "/usr/bin/soffice";
+                    break;
+                case PlatformID.Win32NT:
+                    string binaryDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                    path = Path.Combine(binaryDirectory, "Libre", "program", "soffice.exe");
+                    break;
+                default:
+                    throw new PlatformNotSupportedException("Your Operating System is not supported");
+            }
+            return path;
         }
     }
 }
