cd3b402001fa7e721d7cd3d31e0bcbfa39bcdad1
wopa <p.woltschkow@gmail.com>
2022-05-26 20:37:11 +0200

    add split processor test

---
+++ PdfTool.UnitTests/SplitProcessorTests.cs
@@ -0,0 +1,61 @@
+﻿using iText.Kernel.Pdf;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using PdfTool.Processors.Adapter;
+namespace PdfTool.UnitTests
+{
+    [TestClass]
+    public class SplitProcessorTests
+    {
+        private Mock<IPath> _mockPath;
+        private Mock<IDirectory> _mockDirectory;
+        private Mock<IPdfReader> _mockPdfReader;
+        private Mock<IPdfWriter> _mockPdfWriter;
+        private SplitProcessor _splitProcessor;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _mockPath = new Mock<IPath>();
+            _mockDirectory = new Mock<IDirectory>();
+            _mockPdfReader = new Mock<IPdfReader>();
+            _mockPdfWriter = new Mock<IPdfWriter>();
+
+            _splitProcessor = new SplitProcessor(_mockPath.Object, _mockDirectory.Object, _mockPdfReader.Object, _mockPdfWriter.Object);
+        }
+
+        [TestMethod]
+        public void Split_CreatesDirectoryAndSplitsPdf()
+        {
+            // Arrange
+            var fullPath = "fullPath.pdf";
+            var directory = "directory";
+            var filename = "filename";
+            var outputDirectory = "outputDirectory";
+            var outputPdfPath = "outputPdfPath.pdf";
+            var numberOfPages = 5;
+
+            _mockPath.Setup(p => p.GetDirectoryName(fullPath)).Returns(directory);
+            _mockPath.Setup(p => p.GetFileNameWithoutExtension(fullPath)).Returns(filename);
+            _mockPath.Setup(p => p.Combine(directory, filename)).Returns(outputDirectory);
+            _mockPath.Setup(p => p.Combine(outputDirectory, It.IsAny<string>())).Returns(outputPdfPath);
+
+            var mockPdfDocument = new Mock<IPdfDocument>();
+            mockPdfDocument.Setup(d => d.GetNumberOfPages()).Returns(numberOfPages);
+
+            _mockPdfReader.Setup(r => r.GetPdfDocument(fullPath)).Returns(mockPdfDocument.Object);
+
+            var mockOutputDocument = new Mock<IPdfDocument>();
+            _mockPdfWriter.Setup(w => w.GetPdfDocument(outputPdfPath)).Returns(mockOutputDocument.Object);
+
+            // Act
+            _splitProcessor.Split(fullPath);
+
+            // Assert
+            _mockDirectory.Verify(d => d.CreateDirectory(outputDirectory), Times.Once);
+            _mockPdfReader.Verify(r => r.GetPdfDocument(fullPath), Times.Once);
+            _mockPdfWriter.Verify(w => w.GetPdfDocument(outputPdfPath), Times.Exactly(numberOfPages));
+            mockPdfDocument.Verify(d => d.CopyPagesTo(It.IsAny<int>(), It.IsAny<int>(), mockOutputDocument.Object), Times.Exactly(numberOfPages));
+        }
+    }
+}
