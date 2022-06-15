1ebf8e384005ca95a7bc1a969473030e89db48b0
wopa <p.woltschkow@gmail.com>
2022-06-15 20:45:12 +0200

    refactor compression preocessor

--- PdfTool/Processors/CompressProcessor.cs
+++ PdfTool/Processors/CompressProcessor.cs
@@ -1,26 +1,29 @@
 ﻿using iText.Kernel.Pdf;
+using PdfTool.Processors.Adapter;
 using Path = System.IO.Path;
 
 namespace PdfTool.Processors
 {
-    internal class CompressProcessor
+    public class CompressProcessor
     {
-        public static void Compress(string inputPdfPath)
+        private readonly IPdfReader _pdfReader;
+        private readonly IPdfWriter _pdfWriter;
+
+        public CompressProcessor(IPdfReader pdfReader, IPdfWriter pdfWriter)
+        {
+            _pdfReader = pdfReader;
+            _pdfWriter = pdfWriter;
+        }
+
+        public void Compress(string inputPdfPath)
         {
             var outputPdfPath = $"{Path.GetDirectoryName(inputPdfPath)}/{Path.GetFileNameWithoutExtension(inputPdfPath)}_compressed.pdf";
 
-            var writerProperties = new WriterProperties();
-            writerProperties.SetFullCompressionMode(true);
-            writerProperties.SetCompressionLevel(CompressionConstants.BEST_COMPRESSION);
-
-            using PdfDocument pdfDocument = new PdfDocument(new PdfReader(inputPdfPath));
-            using PdfDocument pdfDocOptimized = new PdfDocument(new PdfWriter(outputPdfPath, writerProperties));
-            pdfDocOptimized.GetWriter().SetSmartMode(true);
-
-
+            using IPdfDocument pdfDocument = _pdfReader.GetPdfDocument(inputPdfPath);
+            using IPdfDocument pdfDocOptimized = _pdfWriter.GetPdfDocument(outputPdfPath, true);
             for (int page = 1; page <= pdfDocument.GetNumberOfPages(); page++)
             {
-                PdfPage pdfPage = pdfDocument.GetPage(page);
+                IPdfPage pdfPage = pdfDocument.GetPage(page);
 
                 pdfDocOptimized.AddPage(pdfDocument.GetPage(page).CopyTo(pdfDocOptimized));
             }
