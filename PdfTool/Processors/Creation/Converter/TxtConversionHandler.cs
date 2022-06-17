b4b3778ddee2c00bd5c8c9b0c4945fd1615cb8dd
wopa <p.woltschkow@gmail.com>
2022-06-17 17:01:23 +0200

    txt conversion handler

--- PdfTool/Processors/Creation/Converter/TxtConversionHandler.cs
+++ PdfTool/Processors/Creation/Converter/TxtConversionHandler.cs
@@ -1,10 +1,23 @@
-﻿namespace PdfTool.Processors.Creation.Converter
+﻿using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using System.IO;
+
+namespace PdfTool.Processors.Creation.Converter
 {
     internal class TxtConversionHandler : IConversionHandler
     {
         public void Convert(string inputString)
         {
-            throw new System.NotImplementedException();
+            string txtContent = File.ReadAllText(inputString);
+            PdfWriter writer = new PdfWriter(inputString);
+            PdfDocument pdf = new PdfDocument(writer);
+
+            Document document = new Document(pdf);
+
+            document.Add(new Paragraph(txtContent));
+
+            document.Close();
         }
     }
 }
