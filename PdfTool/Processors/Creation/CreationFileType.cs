41229e7087e8b7d2727487a1ba4694f60bfba637
wopa <p.woltschkow@gmail.com>
2022-07-23 20:34:56 +0200

    remove unused conversion possibilies for version 1

--- PdfTool/Processors/Creation/CreationFileType.cs
+++ PdfTool/Processors/Creation/CreationFileType.cs
@@ -11,10 +11,6 @@
 
         public static CreationFileType Txt = new CreationFileType("txt");
         public static CreationFileType Word = new CreationFileType("doc", "docx");
-        public static CreationFileType Excel = new CreationFileType("xls", "xlsx");
-        public static CreationFileType PowerPoint = new CreationFileType("ppt", "pptx");
         public static CreationFileType Image = new CreationFileType("jpeg", "jpg", "png");
-        public static CreationFileType Tiff = new CreationFileType("tiff");
-        public static CreationFileType Rtf = new CreationFileType("rtf");
     }
 }
