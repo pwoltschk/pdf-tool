94abb32f44a863a42e867e1b88dae8bcaa0142d1
wopa <p.woltschkow@gmail.com>
2022-08-29 17:01:23 +0200

    base categorization

--- PdfTool/Program.cs
+++ PdfTool/Program.cs
@@ -1,21 +1,43 @@
 ﻿using PdfTool.CLI;
+using System;
 using System.Threading.Tasks;
 
 namespace PdfTool
 {
-    internal class Program
+    class Program
     {
         static async Task Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                await new HelpCommand().ExecuteAsync(null);
+                return;
+            }
+
             switch (args[0])
             {
-                case "help":
                 case "--help":
                 case "-h":
+                case "help":
                     await new HelpCommand().ExecuteAsync(null);
-                break;
+                    break;
+                case "--version":
+                case "-v":
+                case "version":
+                    //ShowVersion();
+                    break;
+                case "compress":
+                   // Compress(args);
+                    break;
+                case "create":
+                   // Create(args);
+                    break;
+                case "remove":
+                   // Remove(args);
+                    break;
                 default:
-                return;
+                    Console.WriteLine($"Unknown command: {args[0]}");
+                    break;
             }
         }
     }
