df81161ba87231b9d7753041ffbe2c18c99e7a9e
wopa <p.woltschkow@gmail.com>
2022-08-29 18:12:34 +0200

    version command dummy

---
+++ PdfTool/CLI/VersionCommand.cs
@@ -0,0 +1,20 @@
+﻿using PdfTool.Processors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PdfTool.CLI
+{
+    internal class VersionCommand : ICommand
+    {
+        public async Task ExecuteAsync(ProcessorArgs args)
+        {
+            await Task.Run(() =>
+            {
+                Console.WriteLine("0.0");
+            });
+        }
+    }
+}
